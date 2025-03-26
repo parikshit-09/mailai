900d673d6aa3377b33cd496f3eed8dbe47d95cd0
parikshit-09 <parikshitrathor@gmail.com>
2025-03-26 19:59:13 +0530

    added authentication

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
