0b6065faa0b231bea8e89b17a14bf4a273749eb7
parikshit-09 <parikshitrathor@gmail.com>
2025-03-27 21:49:49 +0530

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
-    <div>
+    <div className="flex h-screen items-center justify-center">
       <Button>Click me</Button>
     </div>
   );
