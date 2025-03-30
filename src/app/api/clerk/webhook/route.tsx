84f8a28f0c4efced1b0bebe8561615053e571124
parikshit-09 <parikshitrathor@gmail.com>
2025-03-30 17:59:37 +0530

    Finally webhook worked

--- src/app/api/clerk/webhook/route.tsx
+++ src/app/api/clerk/webhook/route.tsx
@@ -1,7 +1,24 @@
 //api/clerk/webhook/route.tsx
 
+import { db } from "@/server/db";
+
 export const POST = async (req: Request) => {
   const { data } = await req.json();
   console.log("Webhook data:", data);
+  const emailAddress = data?.email_addresses[0].email_address;
+  const firstName = data?.first_name;
+  const lastName = data?.last_name;
+  const imageUrl = data?.image_url;
+  const id = data?.id;
+
+  await db.user.create({
+    data: {
+      emailAddress: emailAddress,
+      firstName: firstName,
+      lastName: lastName,
+      imageUrl: imageUrl,
+      id: id,
+    },
+  });
   return new Response("Webhook received", { status: 200 });
 };
